382df08e06bdddbf88c082fada9a347a01f70f91
90新技术 <wangmeijuan@touchealth.com>
2020-07-30 22:39:05 +0800

    feat:上传文件功能实现

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import Main from '../views/main'
 import CategoryEdit from '../views/CategoryEdit'
 import CategoryList from '../views/CategoryList'
+import ItemEdit from '../views/ItemEdit'
+import ItemList from '../views/ItemList'
 
 Vue.use(VueRouter)
 
@@ -14,7 +16,10 @@ Vue.use(VueRouter)
     children: [
       { path: '/categories/create', component: CategoryEdit },
       { path: '/categories/edit/:id', component: CategoryEdit, props: true },
-      { path: '/categories/list', component: CategoryList }
+      { path: '/categories/list', component: CategoryList },
+      { path: '/items/create', component: ItemEdit },
+      { path: '/items/edit/:id', component: ItemEdit, props: true },
+      { path: '/items/list', component: ItemList }
     ]
   },
 ]
