52ea262dd458983bfdcd3face17860c16875dc5f
90新技术 <wangmeijuan@touchealth.com>
2020-08-02 14:58:44 +0800

    feat:登陆页面

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '../views/Login'
 import Main from '../views/main'
 import CategoryEdit from '../views/CategoryEdit'
 import CategoryList from '../views/CategoryList'
@@ -7,10 +8,21 @@ import ItemEdit from '../views/ItemEdit'
 import ItemList from '../views/ItemList'
 import HeroEdit from '../views/HeroEdit'
 import HeroList from '../views/HeroList'
+import ArticleEdit from '../views/ArticleEdit'
+import ArticleList from '../views/ArticleList'
+import AdEdit from '../views/AdEdit'
+import AdList from '../views/AdList'
+import AdminUserEdit from '../views/AdminUserEdit'
+import AdminUserList from '../views/AdminUserList'
 
 Vue.use(VueRouter)
 
   const routes = [
+  {
+    path:'/login',
+    component: Login,
+    name: 'Login'
+  },
   {
     path: '/',
     name: 'Main',
@@ -24,7 +36,16 @@ Vue.use(VueRouter)
       { path: '/items/list', component: ItemList },
       { path: '/heroes/create', component: HeroEdit },
       { path: '/heroes/edit/:id', component: HeroEdit, props: true },
-      { path: '/heroes/list', component: HeroList }
+      { path: '/heroes/list', component: HeroList },
+      { path: '/articles/create', component: ArticleEdit },
+      { path: '/articles/edit/:id', component: ArticleEdit, props: true },
+      { path: '/articles/list', component: ArticleList },
+      { path: '/ads/create', component: AdEdit },
+      { path: '/ads/edit/:id', component: AdEdit, props: true },
+      { path: '/ads/list', component: AdList },
+      { path: '/admin_users/create', component: AdminUserEdit },
+      { path: '/admin_users/edit/:id', component: AdminUserEdit, props: true },
+      { path: '/admin_users/list', component: AdminUserList },
     ]
   },
 ]
--- admin/src/views/AdList.vue
+++ admin/src/views/AdList.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="about">
-    <h1>英雄列表</h1>
+    <h1>广告位列表</h1>
     <el-table :data="items">
         <el-table-column prop="_id" label="ID" width="230"></el-table-column>
-        <el-table-column prop="name" label="英雄名称"></el-table-column>
-        <el-table-column prop="title" label="称号"></el-table-column>
-        <el-table-column prop="avatar" label="头像">
-          <template slot-scope="scope">
-            <img :src="scope.row.avatar" alt="" style="height: 3em;">
-          </template>
-        </el-table-column>
+        <el-table-column prop="name" label="名称"></el-table-column>
         <el-table-column fixed="right" label="操作" width="180">
           <template slot-scope="scope">
-            <el-button type="primary" size="small" @click="$router.push(`/heroes/edit/${scope.row._id}`)">编辑</el-button>
+            <el-button type="primary" size="small" @click="$router.push(`/ads/edit/${scope.row._id}`)">编辑</el-button>
             <el-button type="primary" size="small" @click="remove(scope.row)">删除</el-button>
           </template>
         </el-table-column>
@@ -29,17 +23,17 @@ export default {
   },
   methods: {
     async fetch() {
-      const res = await this.$http.get('rest/heroes')
+      const res = await this.$http.get('rest/ads')
       this.items = res.data
     },
     async remove(row) {
-       this.$confirm(`是否确定要删除英雄 "${row.name}"?`, '提示', {
+       this.$confirm(`是否确定要删除 "${row.name}"?`, '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
         }).then(async () => {
           // eslint-disable-next-line
-          const res = await this.$http.delete(`rest/heroes/${row._id}`) 
+          const res = await this.$http.delete(`rest/ads/${row._id}`) 
           this.$message({
             type: 'success',
             message: '删除成功!'
